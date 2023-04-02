e79857e91f94b615c305a295e763f912137d40a6
lucasbotelhoo <lucasbotelho97@hotmail.com>
2023-04-02 03:43:07 +0000

    arquivo .py modificado

--- gasolina.py
+++ gasolina.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns 
+import matplotlib.pyplot as plt
 
-df = pd.read_csv(r'/content/da_ebac/gasolina.csv',sep = ',')
+df = pd.read_csv('gasolina.csv', sep=',')
 
-grafico = sns.barplot(data=df, x='dia', y='venda', ci=None, palette='deep')
+grafico = sns.lineplot(data=df, x='dia', y='venda')
 grafico.set(title='Média do Preço da Gasolina',xlabel='dia',ylabel='price');
 plt.savefig('gasolina.png')
